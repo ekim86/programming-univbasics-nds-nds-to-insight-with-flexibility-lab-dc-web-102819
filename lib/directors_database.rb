82ab2d9ec37b4e9c13efefa107dfb9525d84b0d9
eunice kim <oonis86@gmail.com>
2019-10-25 14:16:30 +0000

    Automatically backed up by Learn

--- lib/directors_database.rb
+++ lib/directors_database.rb
@@ -8,6 +8,31 @@ end
 require 'directors_database'
 
 
+def flatten_a_o_a(aoa)
+  result = []
+  i = 0
+
+  while i < aoa.length do
+    k = 0
+    while k < aoa[i].length do
+      result << aoa[i][k]
+      k += 1
+    end
+    i += 1
+  end
+
+  result
+end
+
+def movie_with_director_name(director_name, movie_data)
+  { 
+    :title => movie_data[:title],
+    :worldwide_gross => movie_data[:worldwide_gross],
+    :release_year => movie_data[:release_year],
+    :studio => movie_data[:studio],
+    :director_name => director_name
+  }
+end
 
 
 def movies_with_director_key(name, movies_collection)
